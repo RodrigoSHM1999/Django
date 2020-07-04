075b4a2cdf335900f19b0892abaeb97abee22553
RodrigoSHM1999 <rhumanma@unsa.edu.pe>
2020-07-04 00:13:35 -0500

    sum

--- Rodrigo/calc/views.py
+++ Rodrigo/calc/views.py
@@ -6,4 +6,8 @@ def home(request):
 	return render(request, 'home.html',{'name':'Rodrigo'})
 
 def add(request):
-	return render(request, 'result.html')	
+
+	val1= int(request.GET["num1"])
+	val2= int(request.GET["num2"])
+	res = val1 + val2
+	return render(request, 'result.html',{'result':res})	
