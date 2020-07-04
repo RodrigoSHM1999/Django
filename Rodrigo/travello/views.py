b9b044b42bc6f3c179f7c068febd49a8c14dd2f1
RodrigoSHM1999 <rhumanma@unsa.edu.pe>
2020-07-04 02:18:21 -0500

    dest-img

--- Rodrigo/travello/views.py
+++ Rodrigo/travello/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
-
+from .models import Destination
 # Create your views here.
 def index(request):
-	return render(request, 'index.html')
+
+	dest1 = Destination()
+	dest1.name='Arequipa'
+	dest1.desc='The city'
+	dest1.img = 'destination_1.jpg'
+	dest1.price=700
+
+	dest2 = Destination()
+	dest2.name='Lima'
+	dest2.desc='The city'
+	dest2.img = 'destination_2.jpg'
+	dest2.price=650
+
+	dest3 = Destination()
+	dest3.name='Trujillo'
+	dest3.desc='The city'
+	dest3.img = 'destination_3.jpg'
+	dest3.price=750
+
+	dests = [dest1,dest2,dest3]
+
+	return render(request, 'index.html',{'dests': 	dests})
